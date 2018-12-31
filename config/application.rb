be11338222b593bdc4401ff37fa86a3b0c3bc6bc
senay <senaygeletaw@gmail.com>
2018-12-31 11:34:30 +0300

    added active-admin-config in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,7 +16,11 @@ module InventoryManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Disable application initialize on precompile (heroku faq: fixes database access error on precompile)
+    config.assets.initialize_on_precompile = false
 
+    # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
+    config.assets.precompile += %w(active_admin.css active_admin/print.css active_admin.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
